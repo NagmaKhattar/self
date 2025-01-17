6ea283b421b1b48ce2a8cb302ab81e833700b8df
NagmaKhattar <nagmakhattar@gmail.com>
2025-01-17 08:25:13 -0500

    changing to master

---
+++ src/arraydemonstration/ArrayDemonstration.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package arraydemonstration;
+
+/**
+ *
+ * @author HP
+ */
+public class ArrayDemonstration {
+
+    /**
+     * @param args
+     * the command
+     * line
+     * arguments
+     * change
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int [] ar=new int [4];
+        for(int i=0; i<=ar.length-1; i++)
+        {
+            ar[i]=(int)(Math.random()*100+99);
+        }
+        System.out.println("Values are");
+        for(int i=0; i<=ar.length-1; i++)
+        {
+            System.out.println(ar[i]);
+        }
+    }
+    
+}
